48f8bf2435e5ff966212e6dd9202cede4e7523d7
lalas51 <67890416+lalas51@users.noreply.github.com>
2020-08-03 19:12:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from telethon import TelegramClient, sync
 from .config import *
 client = TelegramClient(Банан, api_id, api_hash)
 client.start()
+from telethon.tl.functions.photos import UploadProfilePhotoRequest, DeletePhotosRequest
+from datetime import datetime
+from .utils import *
