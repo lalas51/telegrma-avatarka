75571fa01b7b60636dab6d72cc1a4eac972ccb7d
JoNik <67890416+lalas51@users.noreply.github.com>
2020-08-04 00:29:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -31,10 +31,10 @@ async def main():
 
 while True:
    if time_has_changed(prev_update_time):
-       bts = generate_time_image_bytes(datetime.now(args.tz).replace(tzinfo=None))
-       await asyncio client(DeletePhotosRequest(await client.get_profile_photos('me')))
+     bts = generate_time_image_bytes(datetime.now(args.tz).replace(tzinfo=None))
+       await client(DeletePhotosRequest(await client.get_profile_photos('me')))
        file = await client.upload_file(bts)
-       await asyncio client(UploadProfilePhotoRequest(file))
+       await client(UploadProfilePhotoRequest(file))
        prev_update_time = datetime.now()
        time.sleep(1)
             
